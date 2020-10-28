e9acac583f906477ecc9db9257fd70971029090b
Monkeystation <job@monkeystation.nl>
2020-10-28 14:29:08 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,15 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          "/img/header.svg": [
+            "X-Frame-Options: SAMEORIGIN",
+          ],
+        },
+      }
+    }
   ],
 }
